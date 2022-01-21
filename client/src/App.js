ae65026cab1debb567b5d78faad800add804dc78
James Choi <jkaichoi@gmail.com>
2022-01-20 21:27:30 -0700

    created auth view. Login, Register, and started the client side

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/shared/Home';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Nomatch from './components/shared/Nomatch';
+import Navbar from './components/shared/Navbar';
+
+const App = () => (
+  <>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/*' element={<Nomatch />} />
+    </Routes>
+  </>
+)
+
+export default App;
+
+
